6b19f4c9e9b5de9f04486b79a7df0e01c1ddbfea
Mathew Harman <mathew.harman@gmail.com>
2024-12-10 12:28:43 -0500

    fix: aaaa: parse AAAA record to correct field of union

--- lib/rdata/rr_aaaa.c
+++ lib/rdata/rr_aaaa.c
@@ -1,5 +1,5 @@
 #include "tiny_dns.h"
 
 tiny_dns_err tiny_dns_parse_rdata_aaaa(IOReader *buf, struct tiny_dns_rr *rr) {
-    return io_reader_get(buf, rr->rdata.rr_a, rr->rdlength);
+    return io_reader_get(buf, rr->rdata.rr_aaaa, rr->rdlength);
 }
